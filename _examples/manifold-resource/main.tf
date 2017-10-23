a65c6dccbfd44f37fc14e830de06336fe2ab661b
Jelmer Snoeck <jelmer@siphoc.com>
2017-10-23 07:29:55 +0200

    Examples: correct terraform formatting.
    Use terraform fmt.

--- _examples/manifold-resource/main.tf
+++ _examples/manifold-resource/main.tf
@@ -1,44 +1,44 @@
 provider "manifold" {
-    // uses the default and loads the API key from the ENV `MANIFOLD_API_KEY`.
+  // uses the default and loads the API key from the ENV `MANIFOLD_API_KEY`.
 }
 
 data "manifold_resource" "example1" {
-    project = "manifold-terraform"
-    resource = "custom-resource1"
+  project  = "manifold-terraform"
+  resource = "custom-resource1"
 
-    credential {
-        key = "TOKEN_ID"
-    }
+  credential {
+    key = "TOKEN_ID"
+  }
 
-    credential {
-        name = "secret"
-        key = "TOKEN_SECRET"
-    }
+  credential {
+    name = "secret"
+    key  = "TOKEN_SECRET"
+  }
 
-    credential {
-        name = "default-example"
-        key = "DEFAULT_EXAMPLE"
-        default = "default-value"
-    }
+  credential {
+    name    = "default-example"
+    key     = "DEFAULT_EXAMPLE"
+    default = "default-value"
+  }
 }
 
 data "manifold_resource" "example2" {
-    project = "manifold-terraform"
-    resource = "custom-resource1"
+  project  = "manifold-terraform"
+  resource = "custom-resource1"
 }
 
 output "TOKEN_ID" {
-    value = "${data.manifold_resource.example1.credentials.TOKEN_ID}"
+  value = "${data.manifold_resource.example1.credentials.TOKEN_ID}"
 }
 
 output "TOKEN_SECRET" {
-    value = "${data.manifold_resource.example1.credentials.secret}"
+  value = "${data.manifold_resource.example1.credentials.secret}"
 }
 
 output "DEFAULT" {
-    value = "${data.manifold_resource.example1.credentials.default-example}"
+  value = "${data.manifold_resource.example1.credentials.default-example}"
 }
 
 output "TOKEN_SECRET_2" {
-    value = "${data.manifold_resource.example2.credentials.TOKEN_SECRET}"
+  value = "${data.manifold_resource.example2.credentials.TOKEN_SECRET}"
 }
