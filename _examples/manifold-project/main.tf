069fef0646f0495b1189e87d27b60cc243a7ce53
Jelmer Snoeck <jelmer@siphoc.com>
2017-10-24 17:31:46 +0200

    Examples: use underscores instead of dashes.

--- _examples/manifold-project/main.tf
+++ _examples/manifold-project/main.tf
@@ -4,33 +4,33 @@ provider "manifold" {
 
 // This example shows how to get all credentials for a project in one go. The
 // setup contains 2 custom resources, `custom-resource1` and `custom-resource2`.
-// `custom-resource1` has 2 credentials, `TOKEN_ID` and `TOKEN_SECRET`.
-// `custom-resource2` has 2 credentials, `USERNAME` and `PASSWORD`.
-data "manifold_project" "no-resource-selected" {
+// `custom_resource1` has 2 credentials, `TOKEN_ID` and `TOKEN_SECRET`.
+// `custom_resource2` has 2 credentials, `USERNAME` and `PASSWORD`.
+data "manifold_project" "no_resource_selected" {
   project = "manifold-terraform"
 }
 
 output "nr_token_id" {
-  value = "${data.manifold_project.no-resource-selected.credentials.TOKEN_ID}"
+  value = "${data.manifold_project.no_resource_selected.credentials.TOKEN_ID}"
 }
 
 output "nr_token_secret" {
-  value = "${data.manifold_project.no-resource-selected.credentials.TOKEN_SECRET}"
+  value = "${data.manifold_project.no_resource_selected.credentials.TOKEN_SECRET}"
 }
 
 output "nr_username" {
-  value = "${data.manifold_project.no-resource-selected.credentials.USERNAME}"
+  value = "${data.manifold_project.no_resource_selected.credentials.USERNAME}"
 }
 
 output "nr_password" {
-  value = "${data.manifold_project.no-resource-selected.credentials.USERNAME}"
+  value = "${data.manifold_project.no_resource_selected.credentials.USERNAME}"
 }
 
 // This example shows how to get all credentials for a specific set of
 // resources, in this case only 1.
 // The difference with the example above is that the `TOKEN_ID` and
 // `TOKEN_SECRET` are not available anymore.
-data "manifold_project" "resource-selected" {
+data "manifold_project" "resource_selected" {
   project = "manifold-terraform"
 
   resource {
@@ -39,11 +39,11 @@ data "manifold_project" "resource-selected" {
 }
 
 output "sr_username" {
-  value = "${data.manifold_project.resource-selected.credentials.USERNAME}"
+  value = "${data.manifold_project.resource_selected.credentials.USERNAME}"
 }
 
 output "sr_password" {
-  value = "${data.manifold_project.resource-selected.credentials.PASSWORD}"
+  value = "${data.manifold_project.resource_selected.credentials.PASSWORD}"
 }
 
 // This example shows how to filter for specific credentials across different
@@ -53,14 +53,14 @@ output "sr_password" {
 // with `my-alias`. We also get a credential that hasn't been set,
 // `NON_EXISTING`and give it a default value `my-default-secret`. Lastly, we
 // just select the `TOKEN_ID` from `custom-resource1`.
-data "manifold_project" "credential-selected" {
+data "manifold_project" "credential_selected" {
   project = "manifold-terraform"
 
   resource {
     resource = "custom-resource2"
 
     credential {
-      name = "my-alias"
+      name = "my_alias"
       key  = "USERNAME"
     }
 
@@ -80,13 +80,13 @@ data "manifold_project" "credential-selected" {
 }
 
 output "cs_username" {
-  value = "${data.manifold_project.credential-selected.credentials.my-alias}"
+  value = "${data.manifold_project.credential_selected.credentials.my_alias}"
 }
 
 output "cs_non_existing" {
-  value = "${data.manifold_project.credential-selected.credentials.NON_EXISTING}"
+  value = "${data.manifold_project.credential_selected.credentials.NON_EXISTING}"
 }
 
 output "cs_token_id" {
-  value = "${data.manifold_project.credential-selected.credentials.TOKEN_ID}"
+  value = "${data.manifold_project.credential_selected.credentials.TOKEN_ID}"
 }
