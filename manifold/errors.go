8111d4199ef8ccb46fa4d7195ff32adfe8a12700
David Harrigan <davidharrigan@users.noreply.github.com>
2019-04-08 19:40:17 -0400

    update error message to appease the linter: warning: error strings should not be capitalized (ST1005) (staticcheck)

--- manifold/errors.go
+++ manifold/errors.go
@@ -3,6 +3,6 @@ package manifold
 import "errors"
 
 var (
-	errAPITokenRequired = errors.New("An API Token is required to use this provider")
-	errProjectNotFound  = errors.New("Could not find project for label")
+	errAPITokenRequired = errors.New("an API Token is required to use this provider")
+	errProjectNotFound  = errors.New("could not find project for label")
 )
